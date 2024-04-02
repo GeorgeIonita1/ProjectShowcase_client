e7c804c6a6f55e4e883990da6c5a61a89a8596f9
george_wsl <g3orrg301@gmail.com>
2024-04-02 19:48:46 +0300

    Isolate image compression logic in hook

--- personalSite/frontend/src/components/molecules/ProjectEditForm.jsx
+++ personalSite/frontend/src/components/molecules/ProjectEditForm.jsx
@@ -7,9 +7,9 @@ import { useContext, useState } from "react";
 import { useFormik } from "formik";
 
 import styles from './projectEditForm.module.scss';
-import { ModalDataContext, ModalDataDispatchContext } from "../../contexts/ModalDataContext";
+import { ModalDataContext } from "../../contexts/ModalDataContext";
 import { deleteType, visibilityType } from '../../helpers';
-import ApiServices from "../../services/ApiServices";
+import { useCompressImage } from "../../hooks/useCompressImage";
 
 const MAX_WIDTH = 600;
 const MAX_HEIGHT = 600;
@@ -17,12 +17,8 @@ const MAX_HEIGHT = 600;
 // eslint-disable-next-line react/prop-types
 export default function ProjectEditForm({ onSubmitForm }) {
     const modalData = useContext(ModalDataContext);
-    const dispatch = useContext(ModalDataDispatchContext);
-    const [imageUrl, setImageUrl] = useState(modalData?.data?.imgURL);
     const [loading, setIsLoading] = useState(false);
-
-    const [originalSize, setOriginalSize] = useState('');
-    const [compressedSize, setCompressedSize] = useState('');
+    const { imageUrl, originalSize, compressedSize, setImageUrl, handleCompressImage } = useCompressImage();
 
     const formik = useFormik({
         initialValues: {
@@ -38,67 +34,8 @@ export default function ProjectEditForm({ onSubmitForm }) {
 
     const handleImageUpload = event => {
         setIsLoading(true);
-
-        if (event?.target?.files[0]) {
-            const file = event.target.files[0];
-            const blobURL = URL.createObjectURL(file);
-
-            const img = new Image();
-            img.src = blobURL;
-
-            img.addEventListener('load', () => {
-                const [newWidth, newHeight] = calculateSize(img, MAX_WIDTH, MAX_HEIGHT);
-                const canvas = document.createElement('canvas');
-                canvas.width = newWidth;
-                canvas.height = newHeight;
-                const ctx = canvas.getContext('2d');
-
-                ctx.drawImage(img, 0, 0, newWidth, newHeight);
-                canvas.toBlob(async blob => {
-                    setOriginalSize(readableBytes(file.size));
-                    setCompressedSize(readableBytes(blob.size));
-
-                    const response = await ApiServices.uploadImage(blob);
-
-                    if (response.ok) {
-                        const responseJson = await response.json();
-
-                        dispatch({
-                            type: modalData.requestType,
-                            data: { ...modalData.data, imgURL: responseJson.url },
-                        })
-
-                        setImageUrl(responseJson.url);
-                        setIsLoading(false);
-                    }
-                })
-            })
-        }
-    }
-
-    function calculateSize(img, maxWidth, maxHeight) {
-        let width = img.width;
-        let height = img.height;
-
-        if (width > height) {
-            if (width > maxWidth) {
-                height = Math.round((height * maxWidth) / width);
-                width = maxWidth;
-            }
-        } else {
-            if (height > maxHeight) {
-                width = Math.round((width * maxHeight) / height);
-                height = maxHeight;
-            }
-        }
-        return [width, height];
-    }
-
-    function readableBytes(bytes) {
-        const i = Math.floor(Math.log(bytes) / Math.log(1024)),
-            sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-
-        return (bytes / Math.pow(1024, i)).toFixed(2) + ' ' + sizes[i];
+        handleCompressImage(event)
+        setIsLoading(false);
     }
 
     return (modalData.requestType === deleteType || modalData.requestType === visibilityType) ? (
---
+++ personalSite/frontend/src/hooks/useCompressImage.js
@@ -0,0 +1,80 @@
+import { useContext, useState } from "react";
+
+import { ModalDataDispatchContext } from "../contexts/ModalDataContext";
+import ApiServices from "../services/ApiServices";
+
+
+const MAX_WIDTH = 600;
+const MAX_HEIGHT = 600;
+
+export function useCompressImage(modalData) {
+    const dispatch = useContext(ModalDataDispatchContext);
+
+    const [imageUrl, setImageUrl] = useState(modalData?.data?.imgURL);
+    const [originalSize, setOriginalSize] = useState('');
+    const [compressedSize, setCompressedSize] = useState('');
+
+    const handleCompressImage = e => {
+        if (e?.target?.files[0]) {
+            const file = e.target.files[0];
+            const blobURL = URL.createObjectURL(file);
+
+            const img = new Image();
+            img.src = blobURL;
+
+            img.addEventListener('load', () => {
+                const [newWidth, newHeight] = calculateSize(img, MAX_WIDTH, MAX_HEIGHT);
+                const canvas = document.createElement('canvas');
+                canvas.width = newWidth;
+                canvas.height = newHeight;
+                const ctx = canvas.getContext('2d');
+
+                ctx.drawImage(img, 0, 0, newWidth, newHeight);
+                canvas.toBlob(async blob => {
+                    setOriginalSize(readableBytes(file.size));
+                    setCompressedSize(readableBytes(blob.size));
+
+                    const response = await ApiServices.uploadImage(blob);
+
+                    if (response.ok) {
+                        const responseJson = await response.json();
+
+                        dispatch({
+                            type: modalData.requestType,
+                            data: { ...modalData.data, imgURL: responseJson.url },
+                        })
+
+                        setImageUrl(responseJson.url);
+                    }
+                })
+            })
+        }
+    }
+
+    return { imageUrl, originalSize, compressedSize, setImageUrl, handleCompressImage }
+}
+
+function calculateSize(img, maxWidth, maxHeight) {
+    let width = img.width;
+    let height = img.height;
+
+    if (width > height) {
+        if (width > maxWidth) {
+            height = Math.round((height * maxWidth) / width);
+            width = maxWidth;
+        }
+    } else {
+        if (height > maxHeight) {
+            width = Math.round((width * maxHeight) / height);
+            height = maxHeight;
+        }
+    }
+    return [width, height];
+}
+
+function readableBytes(bytes) {
+    const i = Math.floor(Math.log(bytes) / Math.log(1024)),
+        sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+    return (bytes / Math.pow(1024, i)).toFixed(2) + ' ' + sizes[i];
+}
